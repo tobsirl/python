600e482a0e1cfff3c2796b49f4a6ba1a1e06ffcc
Paul Tobin <tobster111@gmail.com>
2020-10-11 16:44:07 +0100

    Math functions

--- course/python-basics/basics.py
+++ course/python-basics/basics.py
@@ -16,17 +16,21 @@ dict
 None
 
 # integers -> whole numbers
-print(2 + 4)
-print(2 - 4)
-print(2 * 4)
-print(2 / 4)
+# print(2 + 4)
+# print(2 - 4)
+# print(2 * 4)
+# print(2 / 4)
 
-# type
-print(type(2 + 4))  # <class 'int'>
-print(type(2 - 4))  # <class 'int'>
-print(type(2 * 4))  # <class 'int'>
-print(type(2 / 4))  # <class 'float'>
+# # type
+# print(type(2 + 4))  # <class 'int'>
+# print(type(2 - 4))  # <class 'int'>
+# print(type(2 * 4))  # <class 'int'>
+# print(type(2 / 4))  # <class 'float'>
 
-print(2 ** 3)  # To the power of
-print(5 // 4)  # 3 / 4 rounded down to an int
-print(5 % 4) # remander of the division
+# print(2 ** 3)  # power of
+# print(5 // 4)  # 3 / 4 rounded down to an int
+# print(5 % 4) # remander of the division
+
+# math functions
+print(round(3.1))
+print(abs(-20))
